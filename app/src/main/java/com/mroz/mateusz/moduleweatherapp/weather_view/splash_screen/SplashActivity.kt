e3f9ea54f9e7df9921a12438992ec810a021083b
Mateusz Mroz <mateusz.mroz.programista@gmail.com>
2019-02-06 21:45:50 +0100

    add splash screen and navigation from AAC part.2

--- app/src/main/java/com/mroz/mateusz/moduleweatherapp/weather_view/splash_screen/SplashActivity.kt
+++ app/src/main/java/com/mroz/mateusz/moduleweatherapp/weather_view/splash_screen/SplashActivity.kt
@@ -1,10 +1,8 @@
 package com.mroz.mateusz.moduleweatherapp.weather_view.splash_screen
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
 import com.mroz.mateusz.moduleweatherapp.R
 import com.mroz.mateusz.moduleweatherapp.weather_view.MainActivity
 
@@ -13,17 +11,18 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-
     }
 
-    @SuppressLint("ResourceType")
+
     override fun onResume() {
         super.onResume()
+        // delete splash screen from back stack
+        // way without navigation architecture
         var intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         startActivity(intent)
         finish()
-
     }
 
 
